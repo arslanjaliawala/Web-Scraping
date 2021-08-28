d5a07e08b9b75b0de956362083c961396da9deb2
Mohammad Arslan <arslanjaliawala@gmail.com>
2021-08-28 21:07:00 +0500

    This has 3 sample apis

---
+++ firstapi.py
@@ -0,0 +1,33 @@
+import requests,json, scripts
+
+#response = requests.get("https://randomfox.ca/floof")
+#fox = response.json()
+#print(fox["image"])
+#*******************
+'''
+lat = input("Enter Latitude")
+lon = input("Enter Longitude")
+url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=eb40d142570bee8397413f0fef2a9051"
+jsondata = requests.get(url=url).json()
+main = jsondata['current']['weather'][0]['main']
+description = "//" + jsondata['current']['weather'][0]['description']
+print(main + description)
+'''
+#nearbylocations
+#originlat = input("Please enter latitude of your current destination"
+originlat = '25.3960'
+#originlng = input("Please enter longitude of your current destination"
+originlng = '68.3578'
+#places = input("Please enter places you wish to visit")
+places = "restaurant"
+url = url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+originlat+','+originlng+"&radius=1500&type="+places+"&key=AIzaSyDNroC_odFb_wK-MNGd__dz70ZtJM0ru9o"
+response = requests.get(url = url).json()
+count = 0
+for count in response['results']:
+    name = (count['name'])
+    lat = str(count['geometry']['location']['lat'])
+    lng = str(count['geometry']['location']['lng'])
+    newurl = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+originlat+","+originlng+"&destinations="+ lat +"%2C"+lng+"&key=AIzaSyDNroC_odFb_wK-MNGd__dz70ZtJM0ru9o"
+    response2 = requests.get(url = newurl).json()
+    time = response2['rows'][0]['elements'][0]['duration']['text']
+    print(name + " " + time)
